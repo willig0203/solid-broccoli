9cb3153b478a9a5db02a51c58b2cf55e3f4dbd74
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-09-19 12:45:49 -0500

    server updated ready to test

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,6 +30,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(id: ID!, input: BookInput!): User
+    removeBook(id: ID!, bookId: String!): User
   }
 
   input BookInput {
